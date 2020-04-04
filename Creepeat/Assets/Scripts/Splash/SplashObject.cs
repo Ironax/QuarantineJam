d8a20bc8195cb6003512f6f899742aafd6663845
Mehdi <m.ouzerout@student.isartdigital.com>
2020-04-04 17:53:15 +0200

    [Fix] SplashShader

--- Creepeat/Assets/Scripts/Splash/SplashObject.cs
+++ Creepeat/Assets/Scripts/Splash/SplashObject.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class SplashObject : MonoBehaviour
 {
-    [SerializeField]
     private Texture2D m_texture;
-    [SerializeField]
     private Material m_material;
 
-    private Color color;
-    private int textureWidth = 500;
-    private int textureHeight = 500;
+    private readonly Color color = new Color();
+    private int textureWidth = 512;
+    private int textureHeight = 512;
     private bool isEnabled = false;
     // Start is called before the first frame update
     void Start()
@@ -27,13 +25,29 @@ public class SplashObject : MonoBehaviour
 
     private void SetSplash()
     {
-        Color c_color = Color.black;
-        m_texture = new Texture2D(textureWidth, textureHeight);
-        for (int x = 0; x < textureWidth; ++x)
-            for (int y = 0; x < textureHeight; ++y)
-                m_texture.SetPixel(x, y, c_color);
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer)
+        {
+            foreach (Material material in renderer.materials)
+            {
+                if (material.shader.name == "Custom/SplashEffect")
+                {
+                    m_material = material;
+                    break;
+                }
+            }
+            if (m_material)
+            {
+                m_texture = new Texture2D(textureWidth, textureHeight);
+                for (int x = 0; x < textureWidth; ++x)
+                    for (int y = 0; y < textureHeight; ++y)
+                        m_texture.SetPixel(x, y, color);
+                m_texture.Apply();
 
-        m_material.SetTexture("_DrawingTex", m_texture);
+                m_material.SetTexture("_DrawingTex", m_texture);
+                isEnabled = true;
+            }
+        }
     }
 
     public void PaintOn(Vector2 textureCoord, Texture2D splashTexture)
@@ -42,13 +56,21 @@ public class SplashObject : MonoBehaviour
         {
             int x = (int)(textureCoord.x * textureWidth) - (splashTexture.width / 2);
             int y = (int)(textureCoord.y * textureHeight) - (splashTexture.height / 2);
-            //for (int i = 0; i < splashTexture.width; ++i)
-            //    for (int j = 0; j  0)
-            //    {
-            //        Color result = Color.Lerp(existingColor, targetColor, alpha);   // resulting color is an addition of splash texture to the texture based on alpha
-            //        result.a = existingColor.a + alpha;                             // but resulting alpha is a sum of alphas (adding transparent color should not make base color more transparent)
-            //        m_texture.SetPixel(newX, newY, result);
-            //    }
+            for (int i = 0; i < splashTexture.width; ++i)
+                for (int j = 0; j < splashTexture.height;  j++)
+                {
+                    int newX = x + i;
+                    int newY = y + j;
+                    Color existingColor = m_texture.GetPixel(newX, newY);
+                    Color targetColor = splashTexture.GetPixel(i, j);
+                    float alpha = targetColor.a;
+                    if (alpha > 0)
+                    {
+                        Color result = Color.Lerp(existingColor, targetColor, alpha);   // resulting color is an addition of splash texture to the texture based on alpha
+                        result.a = existingColor.a + alpha;                             // but resulting alpha is a sum of alphas (adding transparent color should not make base color more transparent)
+                        m_texture.SetPixel(newX, newY, result);
+                    }
+                }
         }
 
         m_texture.Apply();
