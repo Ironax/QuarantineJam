0eaa5f4bebb546b64d4c5d8e4ca15c2bc81b747c
Ironax <matthieu.ronarch@gmail.com>
2020-04-05 23:53:45 +0200

    [add] textes

---
+++ Creepeat/Assets/collectorIndicator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class collectorIndicator : MonoBehaviour
+{
+	[SerializeField]
+	TextMesh text;
+	[SerializeField]
+	Collector collector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		text.text = collector.collected + " / 4";
+    }
+}
